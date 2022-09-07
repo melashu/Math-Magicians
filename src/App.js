c5c62e4b5803af57a85f5b7c15b0976eb7b3d486
melashu <meshu102@outlook.com>
2022-09-07 16:53:51 +0300

    Add Calculator componenet

--- src/App.js
+++ src/App.js
@@ -1,13 +1,7 @@
-import './App.css';
+import Calculator from './components/Calculator';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <h1>Welcome to Math Magician App</h1>
-      </header>
-    </div>
-  );
+  return <Calculator />;
 }
 
 export default App;
