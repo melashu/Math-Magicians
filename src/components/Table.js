33933d5734567620b469ffb7c972bb7f5ab5b2b7
melashu <meshu102@outlook.com>
2022-09-07 20:02:09 +0300

    fix typo error

--- src/components/Table.js
+++ src/components/Table.js
@@ -17,19 +17,19 @@ class Table extends Component {
         </tr>
         <tr>
           <td>7</td>
-          <td>8/-</td>
+          <td>8</td>
           <td>9</td>
           <td>*</td>
         </tr>
         <tr>
           <td>4</td>
-          <td>5/-</td>
+          <td>5</td>
           <td>6</td>
           <td>-</td>
         </tr>
         <tr>
           <td>1</td>
-          <td>2/-</td>
+          <td>2</td>
           <td>3</td>
           <td>/</td>
         </tr>
