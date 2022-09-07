66544a8b6fc324c0795fe2303aabf0e0dcf8c45c
melashu <meshu102@outlook.com>
2022-09-07 19:58:07 +0300

    update functional componenet to class based componenet

--- src/App.js
+++ src/App.js
@@ -1,7 +1,16 @@
 import Calculator from './components/Calculator';
-
-function App() {
-  return <Calculator />;
+import React from 'react';
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+  state = {  }
+  render() { 
+    return ( 
+      <Calculator/>
+    );
+  }
 }
+ 
 
 export default App;
