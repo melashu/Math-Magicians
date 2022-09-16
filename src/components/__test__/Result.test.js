cb13a74da42892b683f135c1041a7206a2c40ae0
melashu <meshu102@outlook.com>
2022-09-16 12:46:23 +0300

    Add test for result component

---
+++ src/components/__test__/Result.test.js
@@ -0,0 +1,22 @@
+import { screen, render } from "@testing-library/react";
+import "@testing-library/jest-dom";
+import Result from "../Result";
+
+describe("Testing Result component", () => {
+  it("Test heading text is present or not", () => {
+    render(<Result />);
+    const header = screen.queryByRole("heading");
+    expect(header).toHaveTextContent("Your Math Calculator");
+  });
+  it("Testing 45, 9 and * visible or not", () => {
+    render(<Result total={45} next={8} operation="*" />);
+    const container = screen.queryByTestId("result");
+    expect(container).toHaveTextContent("45*8");
+  });
+
+  it("Testing 45, 8 and - visible or not", () => {
+    render(<Result total={45} next={8} operation="-" />);
+    const container = screen.queryByTestId("result");
+    expect(container).toHaveTextContent("45-8");
+  });
+});
