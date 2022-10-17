561449a0304e131d11c67c286ae8ac7ad9bf267d
Meshu <meshu102@outlook.com>
2022-10-17 17:55:05 +0300

    Update Footer.jsx

--- src/pages/Footer.jsx
+++ src/pages/Footer.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import '../css/Footer.css';
 
 const Footer = () => (
-  <footer className="footer">
+  <footer className="footer" style="margin-top:60px">
     <div className="author">
       <strong> Designed by Meshu Amare &copy; 2022</strong>
     </div>
